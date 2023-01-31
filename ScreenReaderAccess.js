9d98a1a8e7c1c5855ceeb9e18a9761bef67c000c
Craig Brett <craigbrett17@aol.com>
2023-01-31 02:26:45 +0000

    Add ! and ? to allowed characters in SR output

--- ScreenReaderAccess.js
+++ ScreenReaderAccess.js
@@ -32,7 +32,7 @@
         var characterRegex = /<\\c\[\d+\](\w+)>/g;
         var playerRegex = /<\\c\[\d+\]\\N\[1\]>/g;
         var escapeSequencesRegex = /\\[nr]/g;
-        var nonTextRegex = /[^\w.,:_ -]+/g;
+        var nonTextRegex = /[^\w.,?!:_ -]+/g;
         return text
             .replace("<WordWrap>", " ")
             .replace("<br>", " ")
