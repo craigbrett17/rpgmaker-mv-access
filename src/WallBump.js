049e805f7a5c66f2e9fdd91c06c48026397de9ad
Craig Brett <craigbrett17@aol.com>
2023-04-28 02:15:04 +0100

    Implemented different sound for bumping into interactab les

--- src/WallBump.js
+++ src/WallBump.js
@@ -8,6 +8,7 @@
     var soundDelay = 500;
     var pauseSound = false;
     var wallBumpSound = { name: "Earth3", pan: 0, pitch: 100, volume: 30 };
+    var interactSound = { name: "Saint5", pan: 0, pitch: 100, volume: 30 };
     
     var overrides = {
         Game_Player_moveStraight: Game_Player.prototype.moveStraight
@@ -16,7 +17,13 @@
     // override the moveStraight to check if the player canPass. If cannot pass, play the sound
     Game_Player.prototype.moveStraight = function(d) {
         if (!pauseSound && !this.canPass(this.x, this.y, d)) {
-            AudioManager.playStaticSe(wallBumpSound);
+            // Check if there's an event at the destination and if it can be activated
+            if (isBumpingInteractable(this.x, this.y, d)) {
+                AudioManager.playStaticSe(interactSound);
+            } else {
+                AudioManager.playStaticSe(wallBumpSound);
+            }
+            
             pauseSound = true;
 
             setTimeout(() => {
@@ -25,5 +32,18 @@
         }
 
         overrides.Game_Player_moveStraight.call(this, d);
+    }
+
+    function isBumpingInteractable(x, y, d) {
+        const x2 = $gameMap.roundXWithDirection(x, d);
+        const y2 = $gameMap.roundYWithDirection(y, d);
+        const events = $gameMap.eventsXy(x2, y2);
+        for (const event of events) {
+            if (event.isTriggerIn([0, 2])) { // "Touch" or "Player Touch"
+                return true;
+            }
+        }
+
+        return false;
     }
 })();
